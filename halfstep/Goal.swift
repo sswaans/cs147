0b57830d95c167f05015e2292c02db8f6915ef6f
stefanswaans <sswaans@stanford.edu>
2017-11-27 14:23:58 -0800

    Shitty Goal class

---
+++ halfstep/Goal.swift
@@ -0,0 +1,30 @@
+//
+//  Goal.swift
+//  halfstep
+//
+//  Created by Stefan Swaans on 11/27/17.
+//  Copyright © 2017 CS 147. All rights reserved.
+//
+
+import Foundation
+
+class Goal {
+    private var name: String
+    // private var levels: [Level]
+//    private var progress { // fix me
+//        get{
+//            return 1
+//        }
+//        set{
+//            return 1
+//        }
+//    }
+    
+    init() {
+        name = "poop"
+    }
+    
+    public func doTheThing() -> Int{
+        return 0
+    }
+}
