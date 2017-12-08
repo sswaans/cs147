a2defc33792e393d6ffc7bc306f390af2698ae8a
Khoi Khoi Viet Le <khoile@bran-cl-08-imi3.stanford.edu>
2017-12-08 03:54:56 -0800

    friends and add friends

---
+++ halfstep/FriendTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  FriendTableViewCell.swift
+//  halfstep
+//
+//  Created by Khoi Khoi Viet Le on 12/8/17.
+//  Copyright © 2017 CS 147. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
