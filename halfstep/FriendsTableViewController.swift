791f72189677879a7e947645e6844768e8906e89
stefanswaans <sswaans@stanford.edu>
2017-12-08 15:32:03 -0800

    deleting friends works

--- halfstep/FriendsTableViewController.swift
+++ halfstep/FriendsTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FriendsTableViewController: UITableViewController {
+class FriendsTableViewController: UITableViewController, FriendTableViewCellDelegate {
     var friends = [User]()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,46 +44,18 @@ class FriendsTableViewController: UITableViewController {
         let user = friends[indexPath.row]
         cell.nameLabel.text = user.name
         cell.profileImageView.image = UIImage(named: user.imagePath!)
-        // Configure the cell...
+        cell.cellDelegate = self
+        cell.tag = indexPath.row
 
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func deleteFriendAction(_ tag: Int) {
+        tableView.reloadData()
+        let indexPath = IndexPath(row: tag, section: 0)
+        friends.remove(at: tag)
+        tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
